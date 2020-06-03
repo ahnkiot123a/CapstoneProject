9df28654f3b3c140f4ebb8eae41ee1fafcbce70a
iKioT <anhnguyen265197@gmail.com>
2020-06-04 06:58:06 +0700

    Update 04/06 (login activity)

--- app/src/main/java/com/koit/capstonproject_version_1/View/LoginActivity.java
+++ app/src/main/java/com/koit/capstonproject_version_1/View/LoginActivity.java
@@ -1,10 +1,5 @@
 package com.koit.capstonproject_version_1.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.LayoutTransition;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,8 +7,14 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -23,23 +24,33 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.koit.capstonproject_version_1.R;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private final int timeoutSplash = 1500;//1500 is timeout for the splash
+    private final int TIMEOUT_SPLASH = 1000;//1500 is timeout for the splash
     private static final String TAG_FB = "FacebookLogin";
+    private static final String TAG_PHONE = "PhoneLogin";
+    private static final String VN_COUNTRY_CODE = "+84";
 
 
     private RelativeLayout relativeLayout1, relativeLayoutRoot;
     private FirebaseAuth firebaseAuth;
     private CallbackManager callbackManager;
-    private Button btnFbLogin;
+    private Button btnFbLogin, btnLogin;
     private LoginButton loginButton;
+    private EditText etPhoneNumber, etOTPorPassword;
+    private String phoneNumber, verificationCodeBySystem, codeByUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +67,74 @@ public class LoginActivity extends AppCompatActivity {
         // [START initialize_fblogin]
         // Initialize Facebook Login button
         callbackManager = CallbackManager.Factory.create();
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeByUser = etOTPorPassword.getText().toString().trim();
+                verifyCode(codeByUser);
+            }
+        });
+
+
+    }
+
+    public void sendVerificationOTPToUser(View view) {
+        phoneNumber = etPhoneNumber.getText().toString().trim();
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                VN_COUNTRY_CODE + phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                TaskExecutors.MAIN_THREAD,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationCodeBySystem = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+//            String code = phoneAuthCredential.getSmsCode();
+//            if(code != null){
+//                verifyCode(code);
+//            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Log.d(TAG_PHONE, e.getMessage());
+            Toast.makeText(LoginActivity.this, "Số điện thoại sai!Mời nhập lại", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private void verifyCode(String codeByUser) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCodeBySystem, codeByUser);
+        signInTheUserByCredential(credential);
+        ;
+    }
+
+    private void signInTheUserByCredential(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG_PHONE, task.getException().getMessage());
+                    Toast.makeText(LoginActivity.this, "Mã OTP sai!Vui lòng nhập lại.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void initView() {
@@ -63,6 +142,9 @@ public class LoginActivity extends AppCompatActivity {
         relativeLayoutRoot = findViewById(R.id.relativeLayoutRoot);
         loginButton = findViewById(R.id.login_button);
         btnFbLogin = findViewById(R.id.btnFbLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etOTPorPassword = findViewById(R.id.etOTPorPassword);
     }
 
     private void animatedChangeSplash() {
@@ -74,18 +156,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         };
         relativeLayoutRoot.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-        handler.postDelayed(runnable, timeoutSplash);
+        handler.postDelayed(runnable, TIMEOUT_SPLASH);
     }
 
     public void callRegisterActivity(View view){
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
-    }
-
-    // [START on_start_check_user]
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     public void loginWithFacebook(View view) {
@@ -144,6 +220,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showProgressBar() {
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
--- app/src/main/java/com/koit/capstonproject_version_1/View/RegisterVerifyPhone.java
+++ app/src/main/java/com/koit/capstonproject_version_1/View/RegisterVerifyPhone.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskExecutors;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
