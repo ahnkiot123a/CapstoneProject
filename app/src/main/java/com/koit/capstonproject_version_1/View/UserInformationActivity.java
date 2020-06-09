04273fa4fe359c29a76e80398ddc301acee8e70d
tubase06155 <tubase06155@fpt.edu.vn>
2020-06-09 09:45:20 +0700

    UI update user and password

---
+++ app/src/main/java/com/koit/capstonproject_version_1/View/UserInformationActivity.java
@@ -0,0 +1,16 @@
+package com.koit.capstonproject_version_1.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.koit.capstonproject_version_1.R;
+
+public class UserInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
