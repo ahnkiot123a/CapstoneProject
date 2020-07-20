0f635d50dd550a806e50b8ff9f7f23691dc0333a
Nguyen Duc Luat <luatnd98@gmail.com>
2020-07-20 21:42:32 +0700

    update UI Select product

--- app/src/main/java/com/koit/capstonproject_version_1/View/ListItemInOrderActivity.java
+++ app/src/main/java/com/koit/capstonproject_version_1/View/ListItemInOrderActivity.java
@@ -3,6 +3,7 @@ package com.koit.capstonproject_version_1.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.koit.capstonproject_version_1.R;
 
@@ -12,5 +13,12 @@ public class ListItemInOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_item_in_order);
+    }
+
+    public void back(View view) {
+        onBackPressed();
+    }
+
+    public void searchByBarcode(View view) {
     }
 }
